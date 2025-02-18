ef27eaf76e0837d05069b3ef2f87b03c25e787f4
Nicholas Jones <nicholassjones2003@gmail.com>
2025-02-18 15:30:40 -0600

    Generates Imperfect Maze

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
+using System.Diagnostics;
+using UnityEngine.Analytics;
 
 public class DungeonGenerator : MonoBehaviour
 {
@@ -23,6 +25,9 @@ public class DungeonGenerator : MonoBehaviour
 
     // TO DO: Change this so it clamps to valid values when assigned
     public Vector2Int startPos = new Vector2Int(0, 0);
+
+    // Prob of Random doors being spawned
+    public float roomExtraDoorProb = 0.1f;
 
 
     // Start is called before the first frame update
@@ -51,6 +56,9 @@ public class DungeonGenerator : MonoBehaviour
 
     void MazeGenerator()
     {
+        // Timing how long it took to generate maze (not including rendering rooms)
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
         Stack<Vector2Int> path = new Stack<Vector2Int>();
 
         int k = 0;
@@ -59,7 +67,7 @@ public class DungeonGenerator : MonoBehaviour
         graph.Add(startPos, new Room());
         Vector2Int curRoomKey = startPos;
         
-        while(k<1000)
+        while(k<10000)
         {
             k++;
 
@@ -85,7 +93,7 @@ public class DungeonGenerator : MonoBehaviour
                 if (!graph.ContainsKey(newRoomKey))
                 {
                     graph.Add(newRoomKey, new Room());
-                    Debug.Log("Creating Room at grid Cords: " + newRoomKey.x + " " + newRoomKey.y);
+                    UnityEngine.Debug.Log("Creating Room at grid Cords: " + newRoomKey.x + " " + newRoomKey.y);
                 }
 
                 if(newRoomKey.y >  curRoomKey.y)
@@ -122,7 +130,68 @@ public class DungeonGenerator : MonoBehaviour
                 }
             }
         }
+        PlaceRandomDoors();
         GenerateDungeon();
+
+        // Evaluating time took
+        stopwatch.Stop();
+        UnityEngine.Debug.Log($"Dungeon Generation Took {stopwatch.ElapsedMilliseconds} ms.");
+    }
+
+    // Runs through the maze and places doors. Some rooms get no extra doors, and some may get 1, 2, or 3.
+    private void PlaceRandomDoors()
+    {
+        foreach (var kvp in graph)
+        {
+            if (Random.value < roomExtraDoorProb)
+            {
+                var curRoomKey = kvp.Key;
+
+                List<Vector2Int> neighbors = CheckNeighborsForRandomDoor(kvp.Key);
+                
+                while (neighbors.Count > 0)
+                {
+                    Vector2Int newRoomKey = neighbors[Random.Range(0, neighbors.Count)];
+                    neighbors.Remove(newRoomKey); // Remove 
+
+                    if (newRoomKey.y > curRoomKey.y)
+                    {
+                        // Going up
+                        graph[curRoomKey].neighbors.Add(graph[newRoomKey], (1f, "North"));
+                        graph[newRoomKey].neighbors.Add(graph[curRoomKey], (1f, "South"));
+
+                    }
+                    else if (newRoomKey.y < curRoomKey.y)
+                    {
+                        // Going down
+                        graph[curRoomKey].neighbors.Add(graph[newRoomKey], (1f, "South"));
+                        graph[newRoomKey].neighbors.Add(graph[curRoomKey], (1f, "North"));
+
+                    }
+                    else if (newRoomKey.x > curRoomKey.x)
+                    {
+                        // Going right
+                        graph[curRoomKey].neighbors.Add(graph[newRoomKey], (1f, "East"));
+                        graph[newRoomKey].neighbors.Add(graph[curRoomKey], (1f, "West"));
+
+                    }
+                    else
+                    {
+                        // Going left
+                        graph[curRoomKey].neighbors.Add(graph[newRoomKey], (1f, "West"));
+                        graph[newRoomKey].neighbors.Add(graph[curRoomKey], (1f, "East"));
+
+                    }
+
+                    UnityEngine.Debug.Log("Placing Random Door at Room: " + curRoomKey.x + " " + curRoomKey.y + " Connecting it to Room " + newRoomKey.x + " " + newRoomKey.y);
+
+                    // Decide whether to place another door or not
+                    if (Random.value < roomExtraDoorProb) continue;
+                    else break;
+                }
+                
+            }
+        }
     }
 
     // Checking each neighbor for if a room could exist there and if we haven't already visited that room
@@ -155,6 +224,43 @@ public class DungeonGenerator : MonoBehaviour
         // Check West Neighbor
         var westRoomKey = new Vector2Int(roomKey.x - 1, roomKey.y);
         if (westRoomKey.x >= 0 && !graph.ContainsKey(westRoomKey))
+        {
+            neighbors.Add(westRoomKey);
+        }
+
+        return neighbors;
+    }
+
+    // Checking if we can place a door across a room
+    // Cannot place doors where doors already exist
+    private List<Vector2Int> CheckNeighborsForRandomDoor(Vector2Int roomKey)
+    {
+        List<Vector2Int> neighbors = new List<Vector2Int>();
+
+        // Check North Neighbor (roomKey.y + 1)
+        var northRoomKey = new Vector2Int(roomKey.x, roomKey.y + 1);
+        if (northRoomKey.y < size.y && graph.ContainsKey(northRoomKey) && !graph[roomKey].neighbors.ContainsKey(graph[northRoomKey]))
+        {
+            neighbors.Add(northRoomKey);
+        }
+
+        // Check South Neighbor
+        var southRoomKey = new Vector2Int(roomKey.x, roomKey.y - 1);
+        if (southRoomKey.y >= 0 && graph.ContainsKey(southRoomKey) && !graph[roomKey].neighbors.ContainsKey(graph[southRoomKey]))
+        {
+            neighbors.Add(southRoomKey);
+        }
+
+        // Check East Neighbor
+        var eastRoomKey = new Vector2Int(roomKey.x + 1, roomKey.y);
+        if (eastRoomKey.x < size.x && graph.ContainsKey(eastRoomKey) && !graph[roomKey].neighbors.ContainsKey(graph[eastRoomKey]))
+        {
+            neighbors.Add(eastRoomKey);
+        }
+
+        // Check West Neighbor
+        var westRoomKey = new Vector2Int(roomKey.x - 1, roomKey.y);
+        if (westRoomKey.x >= 0 && graph.ContainsKey(westRoomKey) && !graph[roomKey].neighbors.ContainsKey(graph[westRoomKey]))
         {
             neighbors.Add(westRoomKey);
         }
